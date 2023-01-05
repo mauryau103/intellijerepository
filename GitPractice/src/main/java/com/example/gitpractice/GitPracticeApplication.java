783020dd3074172c0fd85841e9f2634159e64665
umesh <umesh.maurya@neosoftmail.com>
2023-01-05 12:58:22 +0530

    first commit

---
+++ GitPractice/src/main/java/com/example/gitpractice/GitPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.example.gitpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPracticeApplication.class, args);
+        System.out.println("Umesh Maurya");
+    }
+
+}
